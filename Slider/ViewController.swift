70d8f316737dea20cc3f701486ca91219e41159d
Vinicius Romani <vinicius.romani@moobie.com.br>
2018-09-24 17:02:26 -0300

    Initial Commit

---
+++ Slider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slider
+//
+//  Created by Vinicius Romani on 24/09/18.
+//  Copyright © 2018 Vinicius Romani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
